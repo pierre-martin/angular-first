c223a0144f98cc6d0c945c39bb4d86a25fb618d9
Pierre Martin <pierre.martin@viseo.com>
2014-10-17 10:20:13 +0200

    Resting the form in a more proper way.

--- app/views/comments/comments.controller.js
+++ app/views/comments/comments.controller.js
@@ -4,22 +4,30 @@
     'commentsService',
     function ($scope, commentsService) {
       var controller = this;
+      this.comment = {
+        author: '',
+        emailAddress: '',
+        content: ''
+      };
+      this.originalComment = angular.copy(this.comment);
       this.comments = [];
       this.submit = submit;
 
       function submit() {
         var comment = {
-          author: controller.author,
-          emailAddress: controller.emailAddress,
-          content: controller.content
+          author: controller.comment.author,
+          emailAddress: controller.comment.emailAddress,
+          content: controller.comment.content
         };
         commentsService.addComment(comment).then(function () {
           controller.comments.push(comment);
-          controller.author = null;
-          controller.emailAddress = null;
-          controller.content = null;
-          $scope.commentForm.$setPristine();
+          resetForm();
         });
+      }
+
+      function resetForm() {
+        controller.comment = angular.copy(controller.originalComment);
+        $scope.commentForm.$setPristine();
       }
     }]);
 })();
